9fd86dd23264a54413fcbc2df7ed96ce306993a3
Hakeem Shrief <abalsh@ttu.ee>
2019-07-16 09:33:08 +0400

    Delete HomePage Route, currently unnecessary

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE</h1>
-  </div>
-)
+
 
 function App() {
   return (
@@ -18,7 +14,6 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/shop/hats' component={HatsPage} />
       </Switch>
 
     </div>
