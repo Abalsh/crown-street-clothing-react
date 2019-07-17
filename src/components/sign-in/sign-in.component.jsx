7cf8aa9a7e5730aa1da4cddd47cfa030c55affbb
Hakeem Shrief <abalsh@ttu.ee>
2019-07-17 12:20:38 +0400

    Destructure email and password from this.state in SignIn Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -34,15 +34,16 @@ class SignIn extends React.Component {
     }
 
     render() {
+        const { email, password } = this.state;
         return (
             <div className='sign-in'>
                 <h2> I already have an acount</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} label="Email" required />
+                    <FormInput name='email' type='email' value={email} handleChange={this.handleChange} label="Email" required />
             
-                    <FormInput name='password' type='password' value={this.state.password} handleChange={this.handleChange} label="Password" required />
+                    <FormInput name='password' type='password' value={password} handleChange={this.handleChange} label="Password" required />
                     
                     <div className='buttons'>
                     <CustomButton type="submit"> LOGIN </CustomButton>
