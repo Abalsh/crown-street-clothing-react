c9d8507f294be06c7848f4451c18c973f20687bc
Hakeem Shrief <abalsh@ttu.ee>
2019-07-17 11:41:42 +0400

    Create Sign Up Class with Form Input Component and relevant props

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,1 +1,60 @@
 import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.util';
+
+import './sign-up.styles.scss';
+
+class Signup extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+    render() {
+        const { displayName, email, password, confirmPassword } = this.state;
+        return (
+            <div className='signup'>
+                <h2 className='title'>Don't have an Account ?</h2>
+                <span> Sign Up with your email and password </span>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type='text'
+                        name='displayName'
+                        value={displayName}
+                        onChange={this.handleChange}
+                        label='Display Name'
+                    />
+                    <FormInput
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={this.handleChange}
+                        label='Email'
+                    />
+                  <FormInput
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={this.handleChange}
+                        label='Password'
+                    />
+                    <FormInput
+                        type='password'
+                        name='password'
+                        value={confirmPassword}
+                        onChange={this.handleChange}
+                        label='Confirm Password'
+                    />
+                </form>
+            </div>
+        )
+    }
+}
