5cb2ef33b252d11f336d8ca20dc61cecb4ed51e9
Hakeem Shrief <abalsh@ttu.ee>
2019-07-18 18:38:15 +0400

    Create Checkout page and import it to app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.util';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -60,6 +61,7 @@ class App extends React.Component {
           )
           }
            />
+           <Route path='/checkout' component={CheckoutPage}/>
         </Switch>
 
       </div>
