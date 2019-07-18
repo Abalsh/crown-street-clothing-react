9215a9a1bc760617ee938ef6e6654f9906b85126
Hakeem Shrief <abalsh@ttu.ee>
2019-07-18 11:22:34 +0400

    Add Redux actions reducers and action types, toggle cart true and false, test using console using redux logger

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 // combine reducers is now an object which has our userReducer as a property.
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
