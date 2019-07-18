5f9bcaaf40be30f2c80835804bdcbb28c30da2f8
Hakeem Shrief <abalsh@ttu.ee>
2019-07-18 10:56:53 +0400

    Add Styles to Icon for it to look right

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+
+import './cart-icon.styles.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+
+
 
 const CartIcon = () => (
     <div className='cart-icon'>
