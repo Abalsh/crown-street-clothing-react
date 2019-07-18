03017184de3ff2a9967cc0cc5ba2024694e1df93
Hakeem Shrief <abalsh@ttu.ee>
2019-07-18 17:31:44 +0400

    Use Reduce method to accumulate items added to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,14 +6,19 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 
 
-const CartIcon = ({ toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden, itemCount}) => (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon' onClick={toggleCartHidden}/>
-        <span className='item-count'> 0</span>
+        <span className='item-count'> {itemCount}</span>
     </div>
 )
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
+const mapStateToProps = ({ cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity,
+    0
+    )
+})
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
