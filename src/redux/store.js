3b92e539c420f783708cfb715750d2ec2ea184cf
Hakeem Shrief <abalsh@ttu.ee>
2019-07-19 17:19:43 +0400

    Install and Setup Redux-Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import {createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares)); // spread middlewares spreads the methods in logger (which is saved as an array) to the root reducer.
+export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // spread middlewares spreads the methods in logger (which is saved as an array) to the root reducer.
 
+export const persistor = persistStore(store);
 
 export default store;
