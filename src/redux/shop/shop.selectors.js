4e3d56d6683dd6c05587f41dc3944be0b3c5de52
Hakeem Shrief <abalsh@ttu.ee>
2019-07-20 00:16:57 +0400

    Implement Data Normalization for SHOP_DATA.js to increase efficency

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,4 @@
 import {createSelector} from 'reselect';
-// this maps categories to their respective IDs to be able to match it in link
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectShop = state => state.shop;
 
@@ -17,5 +9,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam => 
 createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
 )
