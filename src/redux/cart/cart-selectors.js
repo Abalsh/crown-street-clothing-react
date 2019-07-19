7e5378886d17b50c6919938805a3d5356a8da05f
Hakeem Shrief <abalsh@ttu.ee>
2019-07-19 09:23:37 +0400

    checkout Page with Total calculating item prices

--- src/redux/cart/cart-selectors.js
+++ src/redux/cart/cart-selectors.js
@@ -24,3 +24,11 @@ export const selectCartItemsCount = createSelector(
                     acc + cartItem.quantity,
                 0)
 )
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce(
+        (acc, cartItem) =>
+            acc + cartItem.quantity * cartItem.price,
+        0)
+)
