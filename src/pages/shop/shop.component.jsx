6cf4d1fa3d6a90df6bf3aee09fedcc4f6941d540
Hakeem Shrief <abalsh@ttu.ee>
2019-07-19 18:20:40 +0400

    Simplify the Shop page component by porting its content to a new component called collections-overview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
-import {selectCollections} from '../../redux/shop/shop.selectors';
-const ShopPage  = ({collections}) => {
-        return (
-            <div className="shop-page"> 
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                            <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
+const ShopPage = ({ collections }) => {
+    return (
+        <div className="shop-page">
+            <CollectionOverview />
+        </div>
+    )
+}
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
