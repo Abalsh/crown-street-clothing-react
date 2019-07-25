48ff22abafcf0b2a29df788d7bd98f0a31094d83
Hakeem Shrief <abalsh@ttu.ee>
2019-07-25 12:58:27 +0400

    Create root-saga.js to call all sagas we create

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,8 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './root-reducer';
-import {fetchCollectionsStart} from './shop/shop.sagas'
+
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware()
 
@@ -14,7 +15,7 @@ if ( process.env.NODE_ENV === 'development'){
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // spread middlewares spreads the methods in logger (which is saved as an array) to the root reducer.
 
-sagaMiddleware.run(fetchCollectionsStart)
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store); // persisted version of our store
 
