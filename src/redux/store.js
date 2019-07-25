e4c073064539a3befd92027602dfe3138f61cb45
Hakeem Shrief <abalsh@ttu.ee>
2019-07-25 11:37:51 +0400

    Install Redux Saga and setup in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,19 @@
 import {createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk]; // thunk catches actions that are functions and putting 'dispatch' as a parameter for said functions.
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware]; // thunk catches actions that are functions and putting 'dispatch' as a parameter for said functions.
 if ( process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // spread middlewares spreads the methods in logger (which is saved as an array) to the root reducer.
+
+// sagaMiddleware.run()
 
 export const persistor = persistStore(store); // persisted version of our store
 
