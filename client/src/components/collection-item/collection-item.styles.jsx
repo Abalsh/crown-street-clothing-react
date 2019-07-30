4fe08d631a682079b8fa40a811e93117a972f2f1
Hakeem Shrief <abalsh@ttu.ee>
2019-07-30 17:13:42 +0400

    Add Some Moble Responsiveness with @Media queries

--- client/src/components/collection-item/collection-item.styles.jsx
+++ client/src/components/collection-item/collection-item.styles.jsx
@@ -1,42 +1,56 @@
 import styled from 'styled-components';
 import CustomButton from '../custom-button/custom-button.component';
 
-
 export const CollectionItemContainer = styled.div`
-    width: 22vw;
-    display: flex;
-    flex-direction: column;
-    height: 350px;
-    align-items: center;
-    position: relative;
-
+  width: 22vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  align-items: center;
+  position: relative;
+  &:hover {
+    .image {
+      opacity: 0.8;
+    }
+    button {
+      opacity: 0.85;
+      display: flex;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    width: 40vw;
     &:hover {
-        .image{
-          opacity: 0.8;;
-        }
-        button{
-          opacity: 0.85;
-          display: flex;
-        }
+      .image {
+        opacity: unset;
       }
+      button {
+        opacity: unset;
+      }
+    }
+  }
 `;
 
 export const AddButton = styled(CustomButton)`
-    width: 80%;
-    opacity: 0.7;
-    position: absolute;
-    top: 255px;
-    display: none;
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 255px;
+  display: none;
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    padding: 0 10px;
+  }
 `;
 
 export const BackgroundImage = styled.div`
-    width: 100%;
-    height: 95%;
-    background-size: cover;
-    background-position: center;
-    margin-bottom: 5px;
-    background-image: ${({ imageUrl }) => `url(${imageUrl})`};
-
+  width: 100%;
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 5px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
 
 export const CollectionFooterContainer = styled.div`
