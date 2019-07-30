f05ff648a92595a45abe3537e41183d9fe4ded2a
Hakeem Shrief <abalsh@ttu.ee>
2019-07-30 17:55:32 +0400

    Lazy Load CollectionsOverviewContainer and CollectionsPageContainer in Shop.component.jsx page

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { ShopPageContainer } from './shop.styles';
+import Spinner from '../../components/spinner/spinner.component';
 
-import CollectionPageContainer from '../collection/collection.container';
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/collections-overview/collections-overview.container')
+);
+
+const CollectionPageContainer = lazy(() =>
+  import('../collection/collection.container')
+);
 
 
 // we want match to display the path, which in this case is shop
@@ -15,10 +23,12 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
         fetchCollectionsStart();
     }, [fetchCollectionsStart]) // same concept as app.js, the reason why the array isn't empty is because of a dependency warning.
     return (
-        <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-        </div>
+        <ShopPageContainer>
+            <Suspense fallback={<Spinner/>}>
+                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+            </Suspense>
+        </ShopPageContainer>
     )
 };
 
